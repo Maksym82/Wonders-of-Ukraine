85d4b8e889d7686b4a444ddbe7399602d6984ec2
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-06-05 19:24:42 +0300

    Create slick.js

---
+++ js/slick.js
@@ -0,0 +1,28 @@
+$('.responsive').slick({
+  // dots: true,
+  prevArrow:
+    '<button type="button" class="my-slick-prev"><svg class="slick_arrow"><use href="./images/icons.svg#icon-arrow-left"></use></svg></button>',
+  nextArrow:
+    '<button type="button" class="my-slick-next"><svg class="slick_arrow"><use href="./images/icons.svg#icon-arrow-right"></use></svg></button>',
+  infinite: true,
+  speed: 300,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  slidesToShow: 3,
+  variableWidth: true,
+
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
