d5398e599f5759ed13e2577a2c8679ac6a3a2e57
Maksym <112901851+Maksym82@users.noreply.github.com>
2026-06-10 09:14:33 +0300

    Fix errors

--- js/main.js
+++ js/main.js
@@ -21,3 +21,58 @@ const swiperYachts = new Swiper('.tours-swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// ===== Scroll Up =====
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+scrollToTopButton.addEventListener('click', () => {
+  anime({
+    targets: document.documentElement,
+    scrollTop: 0,
+    duration: 300,
+    easing: 'easeInOutQuad',
+  });
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    scrollToTopButton.style.display = 'block';
+  } else {
+    scrollToTopButton.style.display = 'none';
+  }
+});
+
+// ===== Mobile menu =====
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const mobileMenuLinks = document.querySelectorAll('.mobile-header-link');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    // bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  for (let index = 0; index < mobileMenuLinks.length; index++) {
+    mobileMenuLinks[index].addEventListener('click', toggleMenu);
+  }
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    // bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
