4e654e0cc60b41a1bba99580b2ddb7fc03eab34c
Maksym <112901851+Maksym82@users.noreply.github.com>
2026-06-14 11:16:04 +0300

    Update images #5 section gallery

--- js/main.js
+++ js/main.js
@@ -76,3 +76,27 @@ window.addEventListener('scroll', () => {
     // bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+// ===== Swiper section Gallery =====
+const swiperGallery = new Swiper('.gallery-swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 8,
+    },
+    // when window width is >= 1280px
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
